d18b9ed56d769c73a393b26b01748931418e8fe0
Javier Mamani <J.mamani@eu.aswatson.com>
2021-05-17 02:31:00 +0200

    First project using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Text 1',
+        day: 'Feb 5 2021',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Text 2',
+        day: 'Mar 3 2020',
+        reminder: false,
+    },
+    {
+        id: 3,
+        text: 'Text 3',
+        day: 'Dec 18 2013',
+        reminder: true,
+    }
+  ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
